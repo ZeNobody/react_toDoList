afc982633e564b53ba88724decdf9f00f048007c
ZeNobody <garcia.gab.pf@gmail.com>
2024-07-19 03:05:58 -0300

    criando funcionalidade de busca

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import './App.css'
 import Todo from './components/Todo'
 import TodoForm from './components/TodoForm';
+import Search from './components/Search';
 
 function App() {
   const [todo, setToDos] = useState([
@@ -25,6 +26,8 @@ function App() {
       isCompleted: false,
     },
   ]);
+
+  const [search, setSearch] = useState("");
 
   const addTodo = (text, category) => {
     const newTodos = [
@@ -54,18 +57,23 @@ function App() {
       todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
     );
     setToDos(newTodos);
-  }
+  };
 
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch}/>
       <div className="todo-list">
-        {todo.map((todo) => {
-          return (
-            <Todo key={todo.id} todo={todo} removeTodo={removeTodo} 
-            completeTodo={completeTodo}/>
-          );
-        })}
+        {todo.filter((todo) => 
+          todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((todo) => (
+            <Todo 
+              key={todo.id} 
+              todo={todo} 
+              removeTodo={removeTodo} 
+              completeTodo={completeTodo}/>
+          ))}
       </div>
       <TodoForm addTodo={addTodo} />
     </div>
